f1aec49210d9a9525c852d1d316d4bc431e777b5
ricnef2121 <ricnef2121@gmail.com>
2025-07-29 01:14:24 -0600

    feat: add any, void and unknow types

--- basics/types.ts
+++ basics/types.ts
@@ -1,1 +1,27 @@
-console.log("Hello, TypeScript!");
+let age :  string = "20";
+let agmonths : number = 20;
+let isSenior : boolean = true;
+let person : object = {};
+
+let fruists : Object[] = [{name: " apple"}]
+
+let response : any = "Hola";
+response = 20;
+response = true;
+response = ["hola", 123]
+
+// void indicates no return value
+function greet() : void {
+    console.log("Hello, World!");
+}
+
+//  unknown is a type-safe version of any
+let response2 : unknown = "Hola";
+response2 = true;
+if(response2){
+    console.log(response2);
+}
+
+// null and undefined to represent absence of value
+let response3 = null;
+let response4 = undefined;
--- dist/types.js
+++ dist/types.js
@@ -1,1 +1,22 @@
-console.log("Hello, TypeScript!");
+var age = "20";
+var agmonths = 20;
+var isSenior = true;
+var person = {};
+var fruists = [{ name: " apple" }];
+var response = "Hola";
+response = 20;
+response = true;
+response = ["hola", 123];
+// void indicates no return value
+function greet() {
+    console.log("Hello, World!");
+}
+//  unknown is a type-safe version of any
+var response2 = "Hola";
+response2 = true;
+if (response2) {
+    console.log(response2);
+}
+// null and undefined to represent absence of value
+var response3 = null;
+var response4 = undefined;
